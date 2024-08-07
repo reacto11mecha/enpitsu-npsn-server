c1c9999523bf535e0f21486c460ab34a60d1491d
Ezra Khairan Permana <48118327+reacto11mecha@users.noreply.github.com>
2024-08-07 07:50:55 +0700

    Update nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,4 +1,13 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-  srcDir: "server"
+  srcDir: "server",
+  routeRules: {
+    "/api/school/**": {
+      cors: true, 
+      headers: { 
+        "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "Access-Control-Allow-Origin": "*",
+      } 
+    }
+  }
 });
